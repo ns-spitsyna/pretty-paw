4ddd65efb8f9e4b71d9876b5d4f28e699420d62d
nadya <n.spicyna@movavi.com>
2023-05-07 17:27:47 +0700

    adaptive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './index.html';
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 
 import './index.scss';
+const videoBg = document.querySelector('.video-bg');
+    videoBg.innerHTML = `<source src="/video/video.webm" type="video/webm">
+    <source src="/video/video.mp4" type="video/mp4">
+    `;
